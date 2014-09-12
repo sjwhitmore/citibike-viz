30b774794ef9c29480d825eba16447989bcdce6d
sjwhitmore <whitmore.samantha@gmail.com>
2014-09-12 18:45:31 -0400

    data parser to go from csv to json

---
+++ parse_data.py
@@ -0,0 +1,33 @@
+import cPickle as pickle
+import pandas as pd
+import json
+import csv
+
+#bikedata = pd.DataFrame.from_csv("2013-07 - Citi Bike trip data.csv", header=0, index_col=False)
+    #print bikedata
+    
+def pdconvert(dte):
+#datestring = pd.datetools.parse(dte).strftime('%Y%m%dT%H:%M%SZ')
+    return pd.datetools.parse(dte)
+
+csvfile = open('test.csv', 'r')
+jsonfile = open('test.json', 'w')
+
+fieldnames = ("tripduration","starttime","stoptime","startstationid","startstationname","startstationlatitude","startstationlongitude","endstationid","endstationname","endstationlatitude","end stationlongitude","bikeid","usertype","birthyear","gender")
+reader = csv.DictReader( csvfile, fieldnames)
+linenum = 0
+for row in reader:
+	if linenum != 0:
+		json.dump(row, jsonfile)
+		jsonfile.write(',')
+		linenum != 1
+	else:
+		linenum += 1
+
+
+csvfile.close()
+jsonfile.close()
+with open('test.json', 'rb+') as f:
+    f.seek(0,2)                 # end of file
+    size=f.tell()               # the size...
+    f.truncate(size-1)          # truncate at that size - how ever many characters
